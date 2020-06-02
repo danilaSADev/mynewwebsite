03bed49f5be74e6e21dc214d61b87d65019c6ba8
danilaSADev <61512362+danilaSADev@users.noreply.github.com>
2020-06-02 13:16:43 +0300

    testingtesting123

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+function windowResized() {
+	let width = document.body.clientWidth;
+
+	let nav = document.getElementById("navTag");
+	let article = document.getElementById("firstArticle");
+	if (width <= 650) {
+		nav.hidden = true;
+		article.style.width = '100%';
+	} else {
+		nav.hidden = false;
+		article.style.width = '80%';
+	}
+}
+
+windowResized()
